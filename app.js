e0b9413a4733faa34eb1e8133ff8d5ab562bcc13
Ricky Cuarez <38024198+pintsik998@users.noreply.github.com>
2019-03-26 12:31:42 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+document.getElementById("sort").onclick = sort;
+
+
+function sort() {
+  alert("button was clicked");
+   var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementsByClassName("table");
+  switching = true;
+  
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+   while (switching) {
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      if (Number(x.innerHTML) > Number(y.innerHTML)) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+
+}
+   
